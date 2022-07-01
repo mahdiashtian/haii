7440cd91bba8995f28da7d0ce0f4e01863b3daeb
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2022-07-01 20:05:45 +0430

    اپدیت بخش سیگنال

--- haii/news/signals.py
+++ haii/news/signals.py
@@ -1,6 +1,28 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete , post_save
 import os
+import json
+import redis
+
+
+try:
+	r = redis.StrictRedis(host = "localhost",port = 6379,db = 1,password = "",decode_responses=True)
+except:
+    print("[-] i can`t connect to redis!")
+
+
+
+@receiver(post_save,sender="news.News")
+def run_after_save(instance,**kwargs):
+    if instance.share:
+        result = {
+            'news':instance.news,
+            'image':instance.image.url if instance.image else None,
+            'date_of_send':str(instance.date_of_send),
+
+        }
+        data = json.dumps(result)
+        r.rpush('sender',data)
 
 
 @receiver(post_delete,sender="news.News")
