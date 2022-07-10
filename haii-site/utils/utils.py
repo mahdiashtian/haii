e24347cb0495cd73a1b7d9e4d426ea6d8d1f3c0a
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2022-07-10 19:23:59 +0430

    ساخت تابع تبدیل استرینگ به لیست و دیکشنری

--- haii-site/utils/utils.py
+++ haii-site/utils/utils.py
@@ -1,16 +1,26 @@
 import os
-
+import ast
 
 TEAM_IMAGE_ROOT = 'haii/'
 
 
 def get_file_name(file_name):
     base_name = os.path.basename(file_name)
-    name,ext = os.path.splitext(base_name)
-    return name,ext
+    name, ext = os.path.splitext(base_name)
+    return name, ext
 
 
-def upload_image_path(instance=None,filename=None):
-    name,ext = get_file_name(filename)
+def upload_image_path(instance=None, filename=None):
+    name, ext = get_file_name(filename)
     final_name = f"{instance.name}{ext}"
     return f"{TEAM_IMAGE_ROOT}{instance.name}/{final_name}"
+
+
+def to_dict(value):
+    new_value = {}
+    for i,x in value.items():
+        if isinstance(x,str):
+            x = ast.literal_eval(x)
+            to_dict(x)
+        new_value[i]=x
+    return new_value
