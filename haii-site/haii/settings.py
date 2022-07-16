b1ed2bd5c34f298a8963140279241b1205bb298a
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2022-07-16 14:12:40 +0430

    اپدیت ریز

--- haii-site/haii/settings.py
+++ haii-site/haii/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-97tlvx01y5x@i24%9a@l2_8oa#brs(3oa6f+i8t2_aggb$@akg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
