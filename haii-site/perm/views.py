4d70a32c72c96c34273f0c2b3eae2f071564bc17
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2022-07-17 15:58:03 +0430

    update view permission

--- haii-site/perm/views.py
+++ haii-site/perm/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import Permission
+from log_admin.mixins import LogAdminMixin
 from rest_framework import viewsets, mixins
 
+from .mixins import PermissionMixin
 from .models import Perm
 from .serializers_ import PermissionSerializer, PermSerializer
 
 
-class PermViewSet(viewsets.ModelViewSet):
+class PermViewSet(LogAdminMixin, PermissionMixin, viewsets.ModelViewSet):
     queryset = Perm.objects.all()
     serializer_class = PermSerializer
     model = Perm
