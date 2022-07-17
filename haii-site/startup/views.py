328f1508418bdfd93d70c5f7b5ff174ece695f91
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2022-07-17 15:50:29 +0430

    add startup permission

--- haii-site/startup/views.py
+++ haii-site/startup/views.py
@@ -1,11 +1,12 @@
+from log_admin.mixins import LogAdminMixin
+from perm.mixins import PermissionMixin
 from rest_framework import viewsets
 
-from log_admin.mixins import LogAdminMixin
 from .models import Startup
 from .serializers_ import StartupSerializers
 
 
-class StartupViewSet(LogAdminMixin, viewsets.ModelViewSet):
+class StartupViewSet(LogAdminMixin, PermissionMixin, viewsets.ModelViewSet):
     serializer_class = StartupSerializers
     queryset = Startup.objects.all()
     model = Startup
